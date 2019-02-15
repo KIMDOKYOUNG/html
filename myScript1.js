764d9953bfed7cdfa7d4d6df91685d75cb48540d
KIMDOKYOUNG <42798576+KIMDOKYOUNG@users.noreply.github.com>
2019-02-15 16:22:17 +0900

    Add files via upload

---
+++ myScript1.js
@@ -0,0 +1,9 @@
+function click1(){
+    document.getElementById("demo").innerHTML = "<img src='a.jpg' style='width:50px;height:50px'> 호윤 : 좋은 아침입니다.";
+}
+function click2(){
+    document.getElementById("demo").innerHTML = "<img src='a.jpg' style='width:50px;height:50px'>호윤 : 저녁 같이 드실래요?";
+}
+function click3(messge){
+    window.alert(messge);
+}
